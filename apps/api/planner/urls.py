d5aae43ea99057626135ca8d44c9897dbed16729
Aakash <aakash@merkisys.com>
2026-08-06 15:05:14 +0530

    Update urls.py

--- apps/api/planner/urls.py
+++ apps/api/planner/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('healthz', views.health_check, name='health-check'),
+    path('healthz/', views.health_check, name='health-check-slash'),
     path('trip', views.trip_list_create, name='trip-create'),
     path('trip/', views.trip_list_create, name='trip-create-slash'),
     path('trips', views.trip_list_create, name='trip-list'),
